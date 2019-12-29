61dc01c867aeaaed37f03981dcf70b6c99062b08
yairgueta <yair.gueta@mail.huji.ac.il>
2019-12-29 22:00:36 +0200

    fire works
    :):):):)

--- Assets/Scripts/FlameCounter.cs
+++ Assets/Scripts/FlameCounter.cs
@@ -1,32 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 public class FlameCounter : MonoBehaviour
 {
-    public const float LEVEL_TIME = 120; //in seconds. 
+    public float LEVEL_TIME = 120f; //in seconds. 
     private float counter;
-    private Vector3 startingValues; 
+    private Vector3 startingValues;
     // Start is called before the first frame update
-    void Start()
+    public void Start()
     {
         counter = 0;
-        sta
+        startingValues = new Vector3(transform.localScale.x, transform.localScale.y, transform.localScale.z);
     }
 
     // Update is called once per frame
     void Update()
     {
         counter += Time.deltaTime;
-        transform.localScale -= new Vector3(1, 1, 1) * (1f / LEVEL_TIME) * Time.deltaTime;
-        print(Time.deltaTime);
+        
+        Vector3 newScale = transform.localScale - startingValues * (1f / LEVEL_TIME) * Time.deltaTime;
+        transform.localScale = new Vector3(Math.Max(newScale.x, 0), Math.Max(newScale.y, 0), Math.Max(newScale.z, 0));
         if (counter >= LEVEL_TIME)
         {
-
+            print("GAME OVER!");
         }
-
-
         
     }
-
-
 }
