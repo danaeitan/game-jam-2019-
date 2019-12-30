5a1d11cf7aabdfbe74a548e8c403127c027163d9
helena.gri <helena.gri@gmail.com>
2019-12-30 14:35:59 +0200

    - Updated OVRScreenFader for fade In - refactored GameManager with Level and Game Reload

--- Assets/Oculus/VR/Scripts/Util/OVRScreenFade.cs
+++ Assets/Oculus/VR/Scripts/Util/OVRScreenFade.cs
@@ -122,7 +122,10 @@ public class OVRScreenFade : MonoBehaviour
         StartCoroutine(Fade(0,1));
     }
 
-
+	public void FadeIn()
+	{
+		StartCoroutine(Fade(1, 0));
+	}
 	/// <summary>
 	/// Starts a fade in when a new level is loaded
 	/// </summary>
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,19 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    public Transform playersInitialPosition; //Place a gameObject indicating the initialPosition;
+    public Transform playersInitialPositions; //Place a gameObject indicating the initialPosition;
     public GameObject player; //this is the OVRCameraRig 
     public Canvas mainCanvas;
+    public UnityEvent [] levels;
 
     public GameObject fire;
     public Vector3 STARTING_POS = new Vector3(0.02f, 0.02f, 2f);
     public Vector3 STARTING_SCALE = new Vector3(1, 1, 1);
     public float FIRE_DURATION;
     private Camera mainCamera; //this is the actual camera with the fade option
+    private EnemyManager enemyManager;
+    private PlayerManager playerManager;
+    private int levelCount;
 
     private void Start()
     {
@@ -23,30 +28,55 @@ public class GameManager : MonoBehaviour
         mainCamera = Camera.main;
         mainCanvas.gameObject.SetActive(false);
     }
-    //This is called when: 1. The fire has ceased, 2.The player dropped the oil 3.when the player collided with obsticle 
-    public void OnEndGame()
-    {
-        mainCanvas.gameObject.SetActive(true);
-    }
 
-    //this method is called by button press. 
-    public void OnRestartGame()
+    //This is called when: 1. The fire has ceased, 2.The player dropped the oil 3.when the player collided with obsticle
+    public void OnRestartLevel()
     {
-        StartCoroutine(OnRestartRoutine());
+       mainCanvas.gameObject.SetActive(true);
+        
+       StartCoroutine(OnRestartLevelRoutine());
     }
 
     //play the fade in / out method 
-    private IEnumerator OnRestartRoutine()
+    private IEnumerator OnRestartLevelRoutine()
+    {
+        int currentLevel = levelCount;
+        var fader = mainCamera.GetComponent<OVRScreenFade>();
+        fader.FadeOut();
+        yield return new WaitForSeconds(2f); //the time it takes to fade 
+        levels[currentLevel].Invoke();
+        mainCamera.transform.position = playersInitialPositions.position;
+        fader.FadeIn();
+    }
+
+    //this is called when player succeeds
+    public void OnWinLevel()
+    {
+        levelCount++;
+        StartCoroutine(LoadNextLevel(levelCount));
+    }
+
+    private IEnumerator LoadNextLevel(int levelCount)
     {
         var fader = mainCamera.GetComponent<OVRScreenFade>();
         fader.FadeOut();
         yield return new WaitForSeconds(2f); //the time it takes to fade 
-        Scene scene = SceneManager.GetActiveScene(); 
-        SceneManager.LoadScene(scene.name);
-    }
+        levels[levelCount].Invoke();
+        fader.FadeIn();
 
+    }
     public void OnWinGame()
     {
         mainCanvas.gameObject.SetActive(true);
+        StartCoroutine(OnRestartGameRoutine());
+    }
+
+    IEnumerator OnRestartGameRoutine()
+    {
+        var fader = mainCamera.GetComponent<OVRScreenFade>();
+        fader.FadeOut();
+        yield return new WaitForSeconds(4);
+        Scene scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.name);
     }
 }
