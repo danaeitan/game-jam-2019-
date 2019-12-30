0fc954f286ab2f31ef7ae1b78014957d8d094f16
miriSilverman <59335543+miriSilverman@users.noreply.github.com>
2019-12-30 14:26:22 +0200

    tree falling

--- Assets/treeFalling.cs
+++ Assets/treeFalling.cs
@@ -1,18 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
 
 public class treeFalling : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Transform tree;
+    public Rigidbody rb;
+    public float speed;
+    public Vector3 currentRotation;
+    public Vector3 angleToRotate;
+
+
+    public void Start()
     {
+        currentRotation = new Vector3(currentRotation.x % 360f, currentRotation.y % 360f, currentRotation.z % 360f);
+        angleToRotate = new Vector3(angleToRotate.x % 360f, angleToRotate.y % 360f, angleToRotate.z % 360f);
+        this.transform.eulerAngles = currentRotation;
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        if (currentRotation.x % 360f < 90)
+        {
+            currentRotation = currentRotation + angleToRotate * Time.deltaTime;
+            currentRotation = new Vector3(currentRotation.x % 360f, currentRotation.y % 360f, currentRotation.z % 360f);
+            this.transform.eulerAngles = currentRotation;
+        }
+      
     }
+
 }
